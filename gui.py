bb45338e50cd36ea15a004e4f4cb84743c8ce8e7
Blake <48300180+blakehourigan@users.noreply.github.com>
2023-11-15 20:03:48 -0500

    finalized gui code

--- gui.py
+++ gui.py
@@ -7,23 +7,27 @@ import openpyxl
 import config
 
 class MinesweeperGUI:
-    def __init__(self, master, size, mines, player, loss_window, win_window, logic, AI):
+    def __init__(self, master, size, mines, player, loss_window, win_window, logic, AI, ai_solution=None):
         self.master = master
-        master.title("Minesweeper")
+        if not ai_solution:
+            master.title("Minesweeper")
         self.loss_window = loss_window
         self.win_window = win_window
-        
+        self.ai_solution = ai_solution
         self.logic = logic
         self.AI = AI
-        
+        self.size = size 
         self.player = player
+        self.mines = mines
 
         self.setup_timer(self.master, size)
     
         self.load_image(self.master)
 
         self.setup_grid(self.master, size)
-
+        
+        self.flag_images = {}  # Dictionary to store flag images for AI winows
+        
         # Initialize a grid of buttons with frames
         self.buttons = [[self.create_button(master, row + 1, column, config.button_width, config.button_height, size)
                         for column in range(size)] for row in range(size)]
@@ -37,30 +41,38 @@ class MinesweeperGUI:
         master.minsize(window_width, window_height)
 
         # Center the window
-        self.center_window(window_width, window_height)
-    
-        if player == "AI":
+        self.center_window(window_width, window_height)          
+        if player == "AI" and ai_solution is None:  # This prevents the infinite loop
             self.logic.initialize_board_AI()
+            self.reveal_board()
             ga_thread = threading.Thread(target=self.run_genetic_algorithm)
             ga_thread.start()
+        elif ai_solution is not None:
             self.reveal_board()
+            self.overlay_ai_flags(ai_solution)  # Call this method to display the AI's flags
 
 
     def run_genetic_algorithm(self):
+        start_time = time.time()
         final_solutions = self.AI.run_multiple_ga_iterations(
             config.ITERATIONS, config.POPULATION_SIZE, config.GENERATIONS, 
             config.TOURNAMENT_SIZE, config.MUTATION_RATE, config.CROSSOVER_RATE, 
             self.logic.num_mines, config.ELITISM_COUNT)
+        end_time = time.time()
+        self.AI_exec_time = end_time - start_time
+        for i, solution in enumerate(final_solutions):
+            self.master.after(0, self.show_ai_solution, i, solution)       
 
         # Create a new Excel workbook and select the active sheet
         wb = openpyxl.Workbook()
         sheet = wb.active
 
         # Set headers
-        headers = ["Solution Number", "Flags", "Mines", "Missing Mines", "Fitness", "# Flags in solution", "Total mines", "# Correct Flags", "% Correctly Identified","Max Possible Fitness"]
+        headers = ["Execution Time", "Solution Number", "Flags", "Mines", "Missing Mines", "Fitness", "# Flags in solution", "Total mines", "# Correct Flags", "% Correctly Identified","Max Possible Fitness"]
         sheet.append(headers)
-
+        sheet['A2'] = str(self.AI_exec_time)
         for i, solution in enumerate(final_solutions):
+            # Assuming 'root' is your Tkinter root window and 'ai_solution' is an instance of 'Individual' with the AI's flags
             solution_number = f"Solution {i+1}"
             flags_str = str(solution.flags)
             mine_coords_str = str(self.logic.mine_coords)
@@ -73,13 +85,35 @@ class MinesweeperGUI:
             mines_str = str(self.logic.num_mines)
 
             # Append the data to the sheet
-            sheet.append([solution_number, flags_str, mine_coords_str, missing_mines, fitness_str, flag_num_str, mines_str, correct_flags, pct_correct,self.AI.maximum_fitness()])
-
+            sheet.append(["",solution_number, flags_str, mine_coords_str, missing_mines, fitness_str, flag_num_str, mines_str, correct_flags, pct_correct,self.AI.maximum_fitness()])
         # Save the workbook to a file
         wb.save("genetic_algorithm_results.xlsx")
         print("Data Written...")
-        exit(1)
 
+
+    def show_ai_solution(self, solution_index,ai_solution):
+        """Creates a new window to display the AI's flags."""
+        # Create a Toplevel window
+        top = tk.Toplevel(self.master)
+        top.title(f"AI's Solution #{solution_index + 1}")
+                
+        # Create an instance of MinesweeperGUI inside the new window
+        # Pass the ai_solution to the new GUI
+        ai_gui = MinesweeperGUI(top, self.logic.grid_size, self.logic.num_mines, "AI", 
+                                self.loss_window, self.win_window, self.logic, self.AI, 
+                                ai_solution=ai_solution)
+
+    def overlay_ai_flags(self, ai_solution):
+        """Overlays AI's flags on the board."""
+        for row in range(self.logic.grid_size):
+            for col in range(self.logic.grid_size):
+                button = self.buttons[row][col]
+                # If the current cell is flagged by the AI, overlay the flag image
+                if (row, col) in ai_solution.flags:
+                    if (row, col) not in self.flag_images:
+                        self.flag_images[(row, col)] = tk.PhotoImage(file=config.flag_image)
+                    button.config(relief=tk.RAISED, image=self.flag_images[(row, col)])
+                    button.image = self.flag_images[(row, col)]  # Keep a reference
 
     def create_button(self, master, row, column, width, height, size):
         # Create a frame to hold the button
@@ -132,7 +166,8 @@ class MinesweeperGUI:
                 self.logic.running = False
                 # reveal the board to the player for 5s
                 self.reveal_board()
-                time.sleep(5)  # Consider if this delay is necessary or optimal
+
+                time.sleep(5) 
                 self.loss_window()
             elif cell.is_numbered():
                 self._configure_button(button, cell)
@@ -147,8 +182,6 @@ class MinesweeperGUI:
         for row_index, row_entries in enumerate(self.logic.board):
             for col_index, cell in enumerate(row_entries):
                 self._update_button_if_not_revealed(row_index, col_index, cell)
-        time.sleep(5)  # Consider if this delay is necessary or optimal
-
 
     def _update_button_if_not_revealed(self, row_index, col_index, cell):
         """ helper function to reveal a cell if it has not already been reveleaded before """
@@ -160,10 +193,10 @@ class MinesweeperGUI:
         """ helper function to configure the button to color the cell based on cell type """
         if action:
             if action == 'setflag':
-                button.config(image=self.flag_image, width=config.button_width, height=config.button_height )
+                button.config(image=self.images['flag'], width=config.button_width, height=config.button_height )
             elif action == 'unset_flag':
                 button.config(relief=tk.RAISED)
-                button.config(image=self.flag_image, width=config.button_width, height=config.button_height)
+                button.config(image=self.images['flag'], width=config.button_width, height=config.button_height)
                 button.config(image='')
         else:
             cell_type = cell.get_type()
@@ -171,7 +204,7 @@ class MinesweeperGUI:
             button.config(relief=tk.SUNKEN, state=tk.DISABLED, bg=color)
             if cell.type == 'mine':
                 button.config(relief=tk.RAISED, state=tk.DISABLED, bg=color)
-                button.config(image=self.mine_image)
+                button.config(image=self.images['mine'])
             elif cell.type != 'empty' and cell.type != 'flag':
                 button.config(text=cell.type)
         self.master.update()
@@ -209,8 +242,10 @@ class MinesweeperGUI:
             master.iconphoto(True, tt_img)
 
         # Load the mine image once and keep a reference
-        self.mine_image = tk.PhotoImage(file=config.mine_image)
-        self.flag_image = tk.PhotoImage(file=config.flag_image)
+        self.images = {
+            'mine': tk.PhotoImage(file=config.mine_image),
+            'flag': tk.PhotoImage(file=config.flag_image)
+        }
 
     def setup_grid(self, master, size):
         # Adjust layout for the new elements
